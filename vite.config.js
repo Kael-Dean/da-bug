24ffd02bc2d231db52474e9b0803724dbb23cdf5
Kael-Dean <pattrakup@outlook.com>
2025-09-09 20:58:44 +0700

    8

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',               // เปิดที่ https://storage.googleapis.com/<bucket>/index.html
+  base: './',               // เปิดที่ https://storage.googleapis.com/<bucket>/index.html
   plugins: [react(), tailwindcss()],
   build: { outDir: 'dist' } // ใช้ค่า default: assets จะอยู่ dist/assets
 })
